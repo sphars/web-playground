ae08b8cbc11ea629eca8883681d948d897bc9a30
Spencer Harston <spencer314@gmail.com>
2018-11-28 21:24:06 -0700

    move js to external

--- sidebars/sidebar2.js
+++ sidebars/sidebar2.js
@@ -3,33 +3,39 @@
     This script provides the client-side functionality for the sidebar nav collapse.
 */
 
-
-// Hide submenus
-$('#body-row .collapse').collapse('hide');
-
-// collapse/expand icon
-$('#collapse-icon').addClass('fa-angle-double-left');
-
-// collapse click
-$('[data-toggle=sidebar-collapse]').click(function () {
-    SidebarCollapse();
+$('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('inactive');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
 });
 
-// handles collapsing/expanding the sidebar
-function SidebarCollapse() {
-    $('.menu-collapsed').toggleClass('d-none');
-    $('.sidebar-submenu').toggleClass('d-none');
-    $('.submenu-icon').toggleClass('d-none');
-    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
 
-    // Treating d-flex/d-none on separators with title
-    var SeparatorTitle = $('.sidebar-separator-title');
-    if (SeparatorTitle.hasClass('d-flex')) {
-        SeparatorTitle.removeClass('d-flex');
-    } else {
-        SeparatorTitle.addClass('d-flex');
-    }
+// // Hide submenus
+// $('#body-row .collapse').collapse('hide');
 
-    // Collapse/Expand icon
-    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
-}
+// // collapse/expand icon
+// $('#collapse-icon').addClass('fa-angle-double-left');
+
+// // collapse click
+// $('[data-toggle=sidebar-collapse]').click(function () {
+//     SidebarCollapse();
+// });
+
+// // handles collapsing/expanding the sidebar
+// function SidebarCollapse() {
+//     $('.menu-collapsed').toggleClass('d-none');
+//     $('.sidebar-submenu').toggleClass('d-none');
+//     $('.submenu-icon').toggleClass('d-none');
+//     $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+
+//     // Treating d-flex/d-none on separators with title
+//     var SeparatorTitle = $('.sidebar-separator-title');
+//     if (SeparatorTitle.hasClass('d-flex')) {
+//         SeparatorTitle.removeClass('d-flex');
+//     } else {
+//         SeparatorTitle.addClass('d-flex');
+//     }
+
+//     // Collapse/Expand icon
+//     $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+// }
